da25f5c01b5705d5f8afa525d185632d71e3dbe6
Croccifixio <aizenkrosis@hotmail.com>
2020-01-25 23:36:02 +0100

    Load environment variables on the frontend

--- frontend/actions/index.js
+++ frontend/actions/index.js
@@ -8,7 +8,7 @@ export const clearCountry = (item) => ({ type: COUNTRY_CLEAR, item })
 export const setCountry = (item) => ({ type: COUNTRY_SET, item })
 
 export const fetchCountry = (searchTerm) =>
-  (dispatch) => fetch(`http://localhost:3002/countries/${searchTerm}`).then(
+  (dispatch) => fetch(`${process.env.BACKEND_URL}:${process.env.BACKEND_PORT}/${process.env.COUNTRIES_ENDPOINT}/${searchTerm}`).then(
     async (response) => dispatch(setCountry(await response.json())),
     console.error,
   )
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,11 @@
+require('dotenv').config({ path: '../.env' })
+
 const withStylus = require('@zeit/next-stylus')
 
-module.exports = withStylus({})
+module.exports = withStylus({
+  env: {
+    BACKEND_PORT: process.env.BACKEND_PORT,
+    BACKEND_URL: process.env.BACKEND_URL,
+    COUNTRIES_ENDPOINT: process.env.COUNTRIES_ENDPOINT,
+  }
+})
