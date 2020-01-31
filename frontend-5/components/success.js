00d8e91a211d627e94e389d05bc99a3614b64c45
Croccifixio <aizenkrosis@hotmail.com>
2020-01-31 01:07:36 +0100

    Adjust sign up success text

--- frontend-5/components/success.js
+++ frontend-5/components/success.js
@@ -1,7 +1,7 @@
 const Success = ({ user }) => (
   <div className="success">
     <h1>Great news, {user.name}! You're account has been created.</h1>
-    <p>Please verify you're account by clicking the activation link we sent to your email at <b>{user.email}</b></p>
+    <p>If this was a real service we'd ask you to verify your account by clicking the activation link we sent to your email at <b>{user.email}</b></p>
   </div>
 )
 
