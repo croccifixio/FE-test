c2101ebf4fd8fead3b5e95df1b100dc21af3e62e
Croccifixio <aizenkrosis@hotmail.com>
2020-01-31 01:06:33 +0100

    Fix typo

--- frontend-6/components/success.js
+++ frontend-6/components/success.js
@@ -4,7 +4,7 @@ const Success = ({ user }) => (
       ? <h1>Welcome back, {user.name}!</h1>
       : <div className="token-text">
           <h1>🦄 This is some hidden text 🌈</h1>
-          <p>You're token let's you see it!</p>
+          <p>Your token let's you see it!</p>
           <p>If this were a full blown application, it would give you access so much more.</p>
         </div>
     }
